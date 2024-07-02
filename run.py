aaf4065b746c8edd26a59ed4a5d8fa72fe91ce5b
kmecseki <kmecseki@gmail.com>
2024-07-01 18:09:33 -0700

    Complete changeover

--- run.py
+++ run.py
@@ -1,11 +1,21 @@
-import this
+import geodata
 
-city_pairs = [['Derekegyhaz', 'Szentes'], ['Szentes', 'Csongrad'], ['Szentes', 'Derekegyhaz'], ['Szentes', 'Szeged'], ['Szentes', 'Szeged']]
+#city_pairs = [['Derekegyhaz', 'Szentes'], ['Szentes', 'Csongrad'], ['Szentes', 'Derekegyhaz'], ['Szentes', 'Szeged'], ['Szentes', 'Szeged']]
 
-distances, cities = this.create_distance_matrix(city_pairs, 'export.geojson')
 
-num_cars = 2
-capacity = 2
+city1 = "Csongrád"
+city2 = "Szeged"
 
-routes, distances_traveled, empty_distances_traveled = this.clarke_and_wright
+cities_data = geodata.geodata("export.geojson")
+
+print(cities_data.calculate_distance(city1, city2))
+
+
+
+#distances, cities = this.create_distance_matrix(city_pairs, 'export.geojson')
+
+#num_cars = 2
+#capacity = 2
+
+#routes, distances_traveled, empty_distances_traveled = this.clarke_and_wright
 
